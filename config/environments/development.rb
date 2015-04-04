6f156fcb94a91bccbc3133307bcbbf5b5fe6ce8c
Dragos Calin <calinho_gx1992@yahoo.com>
2015-04-04 18:00:28 +0300

    Initial Devise & Email setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,23 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # ActionMailer Config
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  # Mandrill
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587,
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_API_KEY"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
