bd2beaf9a8147d9aec0dfea840a51439419c14c7
Dragos Calin <calinho_gx1992@yahoo.com>
2015-04-05 16:52:23 +0300

    Style Sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:show]
+  
   devise_for :users
+  resources :users, only: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
