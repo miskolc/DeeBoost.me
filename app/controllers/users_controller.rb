66e98f002c104ef754c0ed6682051515aa2a9054
Dragos Calin <calinho_gx1992@yahoo.com>
2015-04-17 00:54:44 +0300

    Add location form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @location = current_user.locations.first
+    @location ||= Location.new
   end
 end
