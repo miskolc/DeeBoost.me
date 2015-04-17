77dc058e6a5218aedab3627702ac5280e1eada3e
Dragos Calin <calinho_gx1992@yahoo.com>
2015-04-17 15:53:02 +0300

    Use different variables for current and new location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @location = current_user.locations.first
-    @location ||= Location.new
+    @current_location = current_user.locations.first
+    @location = Location.new
+    @location.user_id = current_user.id
   end
 end
