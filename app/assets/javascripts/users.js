ad15538b526d329d8a51e5cf94d29bc94688d236
Dragos Calin <calinho_gx1992@yahoo.com>
2015-04-18 23:53:14 +0300

    Send Geolocation data to the server

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,3 +5,27 @@
 //= require_tree ./application/addons
 //= require_tree ./users_pages
 
+$(document).on('ready page:change', function () {
+  (function(){
+    var $noLocation = $(".no-location"),
+        $form = $("#new_location"),
+        gpsLocation;
+    if($noLocation.length === 1) {
+      console.log("No Location");
+      LocationGetter.init();
+      console.log("Here1");
+      setTimeout(function(){
+        gpsLocation = LocationGetter.location();
+        console.log("Here2");
+        console.log(gpsLocation);
+        $form.find("#location_longitude").val(gpsLocation.longitude);
+        console.log("Here3");
+        $form.find("input#location_latitude").val(gpsLocation.latitude);
+        console.log("Here4");
+        $form.submit();
+      },5000);
+    } else {
+      console.log("Has Location");
+    }
+  })();
+});
