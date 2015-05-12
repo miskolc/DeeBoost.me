2bc4708f76506559aece5eae9f07a5df2e33e38f
Dragos Calin <calinho_gx1992@yahoo.com>
2015-05-12 14:11:14 +0300

    Use DateTime instead of Date

--- db/migrate/20150510193444_create_days.rb
+++ db/migrate/20150510193444_create_days.rb
@@ -1,7 +1,7 @@
 class CreateDays < ActiveRecord::Migration
   def change
     create_table :days do |t|
-      t.date :date
+      t.date :datetime
       t.json :angles
 
       t.timestamps null: false
