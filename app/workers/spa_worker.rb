6769f0a8437934a619ddcfb170eb11f031e2a975
Dragos Calin <calinho_gx1992@yahoo.com>
2015-05-12 14:12:05 +0300

    Use DateTime instead of Date

--- app/workers/spa_worker.rb
+++ app/workers/spa_worker.rb
@@ -7,7 +7,7 @@ class SpaWorker
   def perform(location_id)
     puts 'Doing hard work' + File.join(File.expand_path(File.join(File.dirname(__FILE__))), 'spa.rb')
     location = Location.find(location_id)
-    date = Date.current
+    date = DateTime.now
     angles = SpaLibrary.calculate date.year, date.month, date.day, 3, 44.852, 24.937, 300
     location.days.create date: date, angles: angles
   end
