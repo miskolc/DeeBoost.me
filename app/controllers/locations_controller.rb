e1950a630854b5a7331b209820398e8d7232e239
Dragos Calin <calinho_gx1992@yahoo.com>
2015-05-15 16:20:08 +0300

    Save version using DateTime Objects

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,15 +1,16 @@
 class LocationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :parse_date_time, only: :create
 
   def create
     @location = current_user.locations.build location_params
-    @location.save
-    SpaWorker.perform_async(@location.id)
+    @location.save    
+    @day = @location.days.create date: @date
+    SpaWorker.perform_async(@day.id)
     redirect_to current_user
   end
 
   def show
-    puts params[:date_time]
     @location = Location.where(user_id: current_user.id).first
     respond_to do |format|
       format.json {render json: @location }
@@ -20,5 +21,14 @@ class LocationsController < ApplicationController
 
     def location_params
       params.require(:location).permit(:longitude, :latitude)
+    end
+
+    def parse_date_time
+      date_time = params.require(:date_time).permit(:year, :day, :month, :timezone)
+      @date = DateTime.new
+      @date = @date.change year: date_time[:year].to_i,
+                           day: date_time[:day].to_i,
+                           month: date_time[:month].to_i,
+                           offset: date_time[:timezone]
     end
 end
--- app/workers/spa_worker.rb
+++ app/workers/spa_worker.rb
@@ -4,11 +4,9 @@ require File.join(File.expand_path(File.join(File.dirname(__FILE__))), 'spa.rb')
 class SpaWorker
   include Sidekiq::Worker
 
-  def perform(location_id)
-    puts 'Doing hard work' + File.join(File.expand_path(File.join(File.dirname(__FILE__))), 'spa.rb')
-    location = Location.find(location_id)
-    date = DateTime.now
+  def perform(date_id)
+    date = Day.find(date_id)
     angles = SpaLibrary.calculate date.year, date.month, date.day, 3, 44.852, 24.937, 300
-    location.days.create date: date, angles: angles
+    date.update angles: angles
   end
 end
