806cd5a1e6be1f9a1a69f12c42b008f4827dc1bd
Dragos Calin <calinho_gx1992@yahoo.com>
2015-05-15 17:16:51 +0300

    Save Day in database as numbers instead of date

--- db/migrate/20150510193444_create_days.rb
+++ db/migrate/20150510193444_create_days.rb
@@ -1,7 +1,11 @@
 class CreateDays < ActiveRecord::Migration
   def change
     create_table :days do |t|
-      t.date :datetime
+      t.integer :location_id
+      t.integer :year
+      t.integer :day
+      t.integer :month
+      t.float :timezone
       t.json :angles
 
       t.timestamps null: false
