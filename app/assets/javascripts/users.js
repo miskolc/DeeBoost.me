fefbc847c522ea606d6731f4973430d491311b80
Dragos Calin <calinho_gx1992@yahoo.com>
2015-05-17 01:44:19 +0300

    Put Google Visualization code into it's own module

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,12 +11,13 @@ $(document).on('page:change', function () {
   function(){
     var $noLocation = $(".no-location")
         $form = $("#new_location");
-
+    
     if($noLocation.length === 1) {
       DateTimeGetter.init($form);
       LocationGetter.init($form);
     } else {
       DateTimeGetter.init($form);
+      ChartDrawer.init();
       $form.find(".js-submit").on('click', function(e) {
          e.preventDefault();
          $form.find(".js-submit").unbind('click');
