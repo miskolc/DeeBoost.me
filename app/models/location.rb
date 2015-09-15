a48ed0dd27f328aea88ee81a6558a19738e70b90
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-15 12:33:50 +0300

    Add reverse geocoding for locations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,8 @@
 class Location < ActiveRecord::Base
   belongs_to :user 
   has_many :days
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode, if: ->(obj){ obj.latitude.present? and obj.longitude.present? }
   after_commit :transaction_success
   after_rollback :transaction_failed
 
