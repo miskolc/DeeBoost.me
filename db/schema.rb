f31dfab59dd8792edeadd4e4e67cc2d8bfc5c247
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-19 06:48:57 +0300

    Add IANA timezone field to locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150827224624) do
+ActiveRecord::Schema.define(version: 20150919034704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150827224624) do
     t.boolean  "current_location", default: false
     t.float    "summer_timezone"
     t.float    "winter_timezone"
+    t.string   "timezone"
   end
 
   create_table "users", force: :cascade do |t|
