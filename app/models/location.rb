287be3e4a15fd5f699da7a2a831ca5347e3d41f7
Dragos Calin <calinho_gx1992@yahoo.com>
2015-09-20 18:38:05 +0300

    Remove garbage code from Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -23,18 +23,6 @@ class Location < ActiveRecord::Base
     ActiveSupport::TimeZone[iana_timezone].tzinfo.current_period.utc_total_offset / 3600.0
   end
 
-  # def self.update_days
-  #   new_day = self.new_day
-  #   # For all the current locations of all users
-  #   # compute the solar angles for that day at 00:00 AM
-  #   self.where( current_location: true,
-  #               self.season => new_day[:timezone]
-  #             ).find_each do |location|
-  #               day = location.days.create new_day
-  #               SpaWorker.perform_async day.id
-  #             end
-  # end
-
   def self.update_days
     new_day = self.new_day
     self.where( current_location: true
@@ -68,16 +56,6 @@ class Location < ActiveRecord::Base
       month: new_day_time.month,
       year: new_day_time.year
     }
-  end
-
-  def self.season
-    :summer_timezone
-    # I try to find the country using the latitude and longitude.
-    # Using the country and the summer offset I find limits of the
-    #          Daylight Saving Time for that region
-    # Then I come back here and I set the season to :summer_timezone
-    #          or :winter_timezone based on current date falling
-    #          within these time limits
   end
 
   private
